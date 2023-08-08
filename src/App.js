b9c161a6a81f3a9cb7b5c99d08948afbd9ff667e
crupy <wlxh95@naver.com>
2023-08-08 13:58:52 +0900

    [Feat] 프론트 엔드 설정파일 업데이트
    프론트 엔드 설정파일 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 // routes
@@ -9,9 +9,15 @@ import ThemeProvider from './theme';
 // components
 import { StyledChart } from './components/chart';
 import ScrollToTop from './components/scroll-to-top';
+
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
+
 // ----------------------------------------------------------------------
 
 export default function App() {
+  dayjs.locale('ko');
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <HelmetProvider>
